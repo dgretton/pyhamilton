1f04a6e179c5f2da2b6835ca8dfabda1d5b28a20
Stefan Golas <stefanmgolas@gmail.com>
2023-01-24 20:23:55 -0500

    Iterated version # for HHS response code fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except(IOError, ImportError):
 
 setup(
     name='pyhamilton',
-    version='1.491.9',
+    version='1.491.10',
     packages=find_packages(exclude=['tests*', 'examples*']),
     license='MIT',
     description='Python for Hamilton liquid handling robots',
