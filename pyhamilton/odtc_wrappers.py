b06c643fd4450d11a5ef3e8c0eb4c360bf6d0b6e
Stefan Golas <stefanmgolas@gmail.com>
2023-01-28 01:36:18 -0500

    new odtc functions

--- pyhamilton/odtc_wrappers.py
+++ pyhamilton/odtc_wrappers.py
@@ -24,21 +24,21 @@ def odtc_abort(odtc, device_id, lock_id):
     result = response.return_data[0]
     return result
 
-def odtc_connect(ham, simulation_mode, local_ip, device_ip, device_port):
+def odtc_connect(ham, simulation_mode, local_ip, device_ip, device_port = ''):
     return_field = ['step-return2']
     cmd = ham.send_command(ODTC_CONNECT, LocalIP=local_ip, DeviceIP=device_ip, DevicePort=device_port, SimulationMode=simulation_mode)
     response = ham.wait_on_response(cmd, raise_first_exception=True, timeout=std_timeout, return_data=return_field)
     device_id = int(response.return_data[0])
     return device_id
 
-def odtc_initialize(ham, device_id, lock_id):
+def odtc_initialize(ham, device_id, lock_id = ''):
     return_field = ['step-return2']
     cmd = ham.send_command(ODTC_INIT, DeviceID=device_id, LockID=lock_id)
     response = ham.wait_on_response(cmd, raise_first_exception=True, timeout=std_timeout, return_data=return_field)
     result = response.return_data[0]
     return result
 
-def odtc_close_door(ham, device_id, lock_id):
+def odtc_close_door(ham, device_id, lock_id = ''):
     return_field = ['step-return2']
     cmd = ham.send_command(ODTC_CLOSE, DeviceID=device_id, LockID=lock_id)
     response = ham.wait_on_response(cmd, raise_first_exception=True, timeout=std_timeout, return_data=return_field)
@@ -59,7 +59,11 @@ def odtc_evaluate_error(ham, device_id, lock_id = ''):
     result = response.return_data[0]
     return result
 
-def odtc_execute_method(ham, device_id, method_name, priority, lock_id = ''):
+def odtc_execute_protocol(ham, device_id, method_name, priority, lock_id = ''):
+    
+    if not 0 < priority < 10001:
+        raise ValueError("Date provided can't be in the past")
+    
     return_field = ['step-return2']
     cmd = ham.send_command(ODTC_EXCT, DeviceID=device_id, LockID=lock_id, MethodName=method_name, Priority=priority)
     response = ham.wait_on_response(cmd, raise_first_exception=True, timeout=std_timeout, return_data=return_field)
@@ -88,9 +92,9 @@ def odtc_read_actual_temperature(ham, device_id, lock_id = ''):
     result = response.return_data[0]
     return result
 
-def odtc_reset(ham, device_id, simulation_mode, time_to_wait, str_device_id = '', pms_id = '', lock_id = ''):
+def odtc_reset(ham, device_id, simulation_mode, timeout, str_device_id = '', pms_id = '', lock_id = ''):
     return_field = ['step-return2']
-    cmd = ham.send_command(ODTC_RESET, DeviceID=device_id, LockID=lock_id, SimulationMode=simulation_mode, TimeToWait=time_to_wait, strDeviceID=str_device_id, PMSID=pms_id)
+    cmd = ham.send_command(ODTC_RESET, DeviceID=device_id, LockID=lock_id, SimulationMode=simulation_mode, TimeToWait=timeout, strDeviceID=str_device_id, PMSID=pms_id)
     response = ham.wait_on_response(cmd, raise_first_exception=True, timeout=std_timeout, return_data=return_field)
     result = response.return_data[0]
     return result
