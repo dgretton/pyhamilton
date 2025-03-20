a36371b06ff77068e17d74061dc2074102d53b9d
BenyaPyat <bpyatski@gmail.com>
2025-03-20 16:25:17 -0400

    Rewrote server thread and added persistent mode

--- pyhamilton/run_venus_client.py
+++ pyhamilton/run_venus_client.py
@@ -1,4 +1,20 @@
 from pyhamilton.interface import run_hamilton_process
+from multiprocessing import Process
+import time
+import os
+import signal
 
 if __name__ == '__main__':
-    run_hamilton_process()
+    oem_process = Process(target=run_hamilton_process, args=())
+    oem_process.start()
+    while True:
+        try:
+            time.sleep(1)
+        except:
+             try:
+                os.kill(oem_process.pid, signal.SIGTERM)
+                oem_process.join()
+             except PermissionError:
+                 time.sleep(1)
+
+
